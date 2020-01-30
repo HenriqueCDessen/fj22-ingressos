c5621a31638c28da53b848bcf210ca6a525bc472
Henrique Dessen <henrique.dessen@icloud.com>
2020-01-30 16:40:00 -0300

    atuzalizacoes da ultima aula

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,42 +3,60 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
-
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	private BigDecimal preco;
 	
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+	
+	
+	public Set<Ingresso> getIngressos() {
+		return ingressos;
+	}
+
+	public void setIngressos(Set<Ingresso> ingressos) {
+		this.ingressos = ingressos;
+	}
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
 	@ManyToOne
 	private Sala sala;
 	
 	@ManyToOne
 	private Filme filme;
 	
-	private BigDecimal preco;
-	
 	/**
 	 * @deprecated hibernate only
 	 */
-	public Sessao() {
+	public Sessao () {
+		
 	}
 	
-	public Sessao(LocalTime horario, Filme filme , Sala sala) {
-		this.horario = horario;
-		this.filme = filme;
-		this.sala = sala;	
-		this.preco = sala.getPreco().add(filme.getPreco());
-	}
-
 	public Integer getId() {
 		return id;
 	}
@@ -70,13 +88,19 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	public BigDecimal getPreco() {
-		return preco.setScale(2,RoundingMode.HALF_UP);
-	}
-	
-	public void setPreco() {
-		this.preco = preco;
-	}
-}
 
+	public Sessao(LocalTime horario, Filme filme, Sala sala ) {
+		this.horario = horario;
+		this.sala = sala;
+		this.filme = filme;
+		this.preco = sala.getPreco().add(filme.getPreco());
+	} 	
+	public Map<String, List<Lugar>> getMapaDeLugares(){
+		return sala.getMapaDeLugares();
+	}
+	
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return this.getIngressos().stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+
+}
